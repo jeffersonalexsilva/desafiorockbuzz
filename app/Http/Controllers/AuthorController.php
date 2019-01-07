6907638341641130edb8426ca28bb8f629cb2092
Jefferson Alex <jabsr@cin.ufpe.br>
2019-01-07 04:05:56 -0200

    author controller initial

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -7,5 +7,34 @@ use App\Http\Controllers;
 
 class AuthorController extends Controller
 {
+    public function index()
+    {
+        return Author::all();
+    }
 
+    public function show($id)
+    {
+        return Author::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        return Author::create($request->all());
+    }
+
+    public function update(Request $request, $id)
+    {
+        $author = Author::findOrFail($id);
+        $author->update($request->all());
+
+        return $author;
+    }
+
+    public function delete(Request $request, $id)
+    {
+        $author = Author::findOrFail($id);
+        $author->delete();
+
+        return 204;
+    }
 }
